c0f793ee0c066bc5c259eae30be7e48f413ef0be
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-28 14:15:51 +0200

    fix(workflows): hiii

--- .github/scripts/checkup/convert.sh
+++ .github/scripts/checkup/convert.sh
@@ -1,7 +1,9 @@
 cd data/fluentui-emoji-animated
 for f in *.png; do
-    new="${f%.*}.gif"
-    ffmpeg -loglevel error -y -i "$f" "$new"
-    rm "$f"
-    echo "Finished $f => $new"
+    gif="${f%.*}.uc.gif"
+    compressed="${f%.*}.gif"
+    ffmpeg -loglevel error -y -i "$f" "$gif"
+    gifsicle --colors=64 --use-col=web --lossy=100 --scale 0.8 -O3 "$gif" -o "$compressed"
+    rm "$f" "$gif"
+    echo "Finished $f => $compressed"
 done
